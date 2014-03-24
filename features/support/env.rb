853249b7e7e2f51d0fcd71783b7644b8a7899998
Andrew Premdas <apremdas@yahoo.co.uk>
2014-03-24 19:16:28 +0000

    In process debugging
    Todo::Runner acts as the entry point for the application. This allows us
    to run in process with aruba.
    
    - new invocation of app
    - aruba in_process include
    - Todo::Runner with initializer and execute! method
    
    feature debugs in process

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
 require 'aruba/cucumber'
+require 'aruba/in_process'
 require 'debugger'
 require 'todo'
 
 # see hooks.rb
-
+Aruba::InProcess.main_class = Todo::Runner
+Aruba.process = Aruba::InProcess
---
+++ lib/todo/runner.rb
@@ -0,0 +1,13 @@
+module Todo
+  class Runner
+    def initialize(argv, stdin=STDIN, stdout=STDOUT, stderr=STDERR, kernel=Kernel)
+      @argv, @stdin, @stdout, @stderr, @kernel = argv, stdin, stdout, stderr, kernel
+    end
+
+    def execute!
+      debugger
+      1
+      0
+    end
+  end
+end
